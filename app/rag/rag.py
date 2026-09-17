aedcbc2ae3d9ebdc201781f8d1d75679a43a5f14
amirhossein <amir0hossin0pr@gmail.com>
2026-09-17 16:23:24 +0330

    update : use prompt builder

--- app/rag/rag.py
+++ app/rag/rag.py
@@ -1,47 +1,39 @@
 from app.generation.llm import LLM
+from app.generation.prompt_builder import PromptBuilder
 from app.rag.context_builder import ContextBuilder
 from app.retrieval.retriever import Retriever
 
+
 class RAG:
     def __init__(
-            self,
-            retriever:Retriever,
-            context_builder:ContextBuilder,
-            llm:LLM
-    ):
+        self,
+        retriever: Retriever,
+        context_builder: ContextBuilder,
+        prompt_builder: PromptBuilder,
+        llm: LLM,
+    ) -> None:
         self._retriever = retriever
         self._context_builder = context_builder
+        self._prompt_builder = prompt_builder
         self._llm = llm
 
     def ask(
-            self,
-            query:str,
-            top_k:int = 3
-    )-> str:
+        self,
+        query: str,
+        top_k: int = 3,
+    ) -> str:
         results = self._retriever.retrieve(
             query=query,
-            top_k=top_k
+            top_k=top_k,
         )
 
         context = self._context_builder.build(
-            results=results
+            results
         )
-        prompt = self._build_prompt(
+
+        prompt = self._prompt_builder.build(
             query=query,
-            context=context
+            context=context,
         )
 
-        return self._llm.generate(prompt=prompt)
-
-    def _build_prompt(
-            self,
-            query:str,
-            context:str
-    )-> str:
-        return (
-            "Answer the question using only "
-            "the provided context \n\n"
-            f"Contex:\n {context}\n\n"
-            f"Question:\n{query}\n\n"
-            "Answer:"
-        )
+        return self._llm.generate(prompt)
